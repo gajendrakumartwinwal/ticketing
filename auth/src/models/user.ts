4511090ed243e73908ba39dcf56722120e57233c
Gajendra Kumar <gajendra.kumar@myntra.com>
2020-12-10 02:20:56 +0530

    commit #1

---
+++ auth/src/models/user.ts
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+interface UserAttrs {
+    email: string
+    password: string
+}
+
+interface UserModel extends mongoose.Model<UserDoc> {
+    build(attrs: UserAttrs): UserDoc
+}
+
+interface UserDoc extends mongoose.Document {
+    email: string
+    password: string
+    // createdAt: string
+    // updatedAt: string
+}
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+
+const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
+
+export {User};
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,22 +3,26 @@ import {body, validationResult} from 'express-validator'
 import {Request, Response} from "express";
 import {RequestValidationError} from "../error/RequestValidationError";
 import {DatabaseValidationError} from "../error/DatabaseValidationError";
+import {User} from "../models/user";
+import {BadRequestError} from "../error/BadRequestError";
 
 const router = express.Router()
 
 router.post('/api/users/signup', [
     body('email').isEmail().withMessage('Email must be valid'),
     body('password').trim().isLength({min: 4, max: 20}).withMessage('Password length must be between 4 to 20')
-], (req: Request, res: Response) => {
+], async (req: Request, res: Response) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()){
         throw new RequestValidationError(errors.array());
     }
     const {email, password} = req.body;
-    console.log('Username: ', email, 'Password: ', password);
-
-    throw new DatabaseValidationError();
-    res.send('Hi signup!!!!');
+    const userObj = await User.findOne({email});
+    if(userObj){
+        throw new BadRequestError('Email in use!');
+    }
+    await User.build({email, password}).save();
+    return res.status(201).send({status: 'user created!'});
 });
 
 export {router as signupRouter};
