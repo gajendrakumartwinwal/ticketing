c4f408cd7c21d91a78c5c20d6ec71a356a1d3ec6
Gajendra Kumar <gajendra.kumar@myntra.com>
2020-12-11 00:23:06 +0530

    commit #1

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,9 +1,10 @@
-import express from 'express'
+import express, {Request, Response} from 'express'
 
 const router = express.Router()
 
-router.post('/api/users/signout', (req: any, res: { send: (arg0: string) => void; }) =>{
-    res.send('Hi signout!!!!');
+router.post('/api/users/signout', (req: Request, res: Response) =>{
+    req.session = null;
+    res.send({currentUser: null});
 });
 
 export {router as signoutRouter};
