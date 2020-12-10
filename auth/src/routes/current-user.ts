f3a8b1562e1e63feda97cb427685c9f038861de9
Gajendra Kumar <gajendra.kumar@myntra.com>
2020-12-10 23:54:11 +0530

    commit #1

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,21 @@
-import express from 'express'
+import express, {Request, Response} from 'express'
+import jwt from 'jsonwebtoken';
 
 const router = express.Router()
 
-router.get('/api/users/currentuser', (req: any, res: { send: (arg0: string) => void; }) =>{
-    res.send('Hi Youghlklkl!!!!');
+router.get('/api/users/currentuser', (req: Request, res: Response) => {
+    if (!req.session?.jwt) {
+        return res.send({currentUser: null});
+    }
+    try {
+        const payload = jwt.verify(
+            req.session.jwt,
+            process.env.JWT_KEY!
+        );
+        res.send({currentUser: payload});
+    } catch (err) {
+        res.send({currentUser: null});
+    }
 });
 
 export {router as currentUserRouter};
