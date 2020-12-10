d16ae1ac6260765708a7c911c2e4950526d63fef
Gajendra Kumar <gajendra.kumar@myntra.com>
2020-12-10 22:43:20 +0530

    commit #1

--- auth/src/middleware/validate-request.ts
+++ auth/src/middleware/validate-request.ts
@@ -1,12 +1,10 @@
 import {NextFunction, Request, Response} from "express";
-import {CustomError} from "../error/CustomError";
 import {validationResult} from "express-validator";
-import {DatabaseValidationError} from "../error/DatabaseValidationError";
 import {RequestValidationError} from "../error/RequestValidationError";
 
 const validateRequest = (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
-    if(errors){
+    if(!errors.isEmpty()){
         throw new RequestValidationError(errors.array());
     }
     next();
