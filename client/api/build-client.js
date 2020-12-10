60953d7e37e7b2b5b2781857f8b119d3a33991d4
Gajendra Kumar <gajendra.kumar@myntra.com>
2020-12-11 03:16:42 +0530

    client app

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -6,6 +6,8 @@ export default ({ req }) => {
 
     return axios.create({
       baseURL: 'http://ingress-nginx.kube-system.svc.cluster.local',
+      baseURL: 'http://ingress-nginx.ingress-nginx.svc.cluster.local',
+      baseURL: 'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
       headers: req.headers
     });
   } else {
